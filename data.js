305f476d325f365bf8dbdbc08e3498988ede128b
Julian Gabriel Fita <JulianGabrielFita@Gmail.com>
2025-06-10 13:51:16 +0100

    Initial commit

---
+++ data.js
@@ -0,0 +1,18 @@
+const servicesData = [
+    {
+        title: "Prop creation",
+        category: "prop",
+        description: ["</b>Props</b> are the essential elements that populate your game worlds, adding depth, realism, and opportunities for player interaction. From intricate weaponry and character tools to environmental clutter like furniture, scattered debris, or decorative elements, prop creation is about bringing the small details that make a big difference.</br></br> My process involves a keen eye for detail, understanding the desired art style of your project, and ensuring every asset is optimized for performance within your game engine. I focus on creating props that not only look fantastic but also seamlessly integrate into your game's narrative and gameplay mechanics, contributing to a truly immersive experience for your players."]
+    },
+    {
+        title: "Character modeling",
+        category: "character",
+        description: ["Characters are the heart of many games, serving as the primary connection between players and your virtual world. <b>Character modeling</b> is about bringing these pivotal figures to life, from heroic protagonists and menacing villains to quirky NPCs and diverse creatures. My approach focuses on creating characters that not only embody your artistic vision but are also technically sound and optimized for in-game performance. </br></br> I pay close attention to topology, rigging readiness, and efficient texturing to ensure your characters animate beautifully and perform smoothly. Whether it's a realistic human, a stylized creature, or an epic fantasy hero, I'm dedicated to crafting memorable characters that enhance your game's narrative and captivate players."]
+    },
+    {
+        title: "Environment art",
+        category: "environment",
+        description: ["<b>Environment</b> is the backbone of any immersive game, establishing the mood, scale, and narrative context for players to explore. From sprawling open worlds and intricate cityscapes to desolate ruins and vibrant forests, my goal is to build rich, believable, and optimized environments that draw players deeper into your game. </br></br> I focus on creating compelling scenes that blend artistic vision with technical precision, ensuring seamless asset integration, efficient UV layouts, and smart material usage. Whether it's crafting individual props, designing modular kits, or assembling entire levels, I aim to deliver stunning environments that not only look fantastic but also support gameplay and tell a story, making your game world truly unforgettable."]
+    }
+
+]
